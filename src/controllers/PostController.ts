370a12fd8482d5774a308fa6636d6d3bd16971c0
Sergey Sorokin <s.sorokin@progforce.com>
2019-04-08 23:48:16 +0300

    Nodemon

---
+++ src/controllers/PostController.ts
@@ -0,0 +1,8 @@
+import express from "express";
+
+export default class PostController {
+    public store(req: express.Request, res: express.Response) {
+        // @fixme Clears all previous results and starts a new game
+        res.status(204);
+    }
+}
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import HomeController from "../controllers/HomeController";
+import PostController from "../controllers/PostController";
 
 const routes: express.Router = express.Router();
 
 routes.get("/", (req: express.Request, res: express.Response) => (new HomeController()).index(req, res));
+routes.post("/game", (req: express.Request, res: express.Response) => (new PostController()).store(req, res));
 
 export default routes;
