b673727c0f8baa769f5d4f3a3760a51946dbada0
Sergey Sorokin <s.sorokin@progforce.com>
2019-04-08 23:49:05 +0300

    Post for game

--- src/controllers/PostController.ts
+++ src/controllers/PostController.ts
@@ -3,6 +3,6 @@ import express from "express";
 export default class PostController {
     public store(req: express.Request, res: express.Response) {
         // @fixme Clears all previous results and starts a new game
-        res.status(204);
+        res.status(204).send();
     }
 }
